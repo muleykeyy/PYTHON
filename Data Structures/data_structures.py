fca8e2324913222d638dc4569e04488242dd929e
Hilal Müleyke Yüksel <hilal.yuksel@vbt.com.tr>
2022-06-27 11:37:03 +0300

    All Variables

---
+++ Data Structures/data_structures.py
@@ -0,0 +1,52 @@
+###############################################
+
+# DATA STRUCTURES
+
+###############################################
+
+# 1) NUMBERS
+
+# Integer:
+h=97
+type(97)
+h*5
+h/9
+h ** 2
+# Float:
+m=97.05
+type(m)
+
+# Complex:
+y=5j+8
+type(y)
+
+# 2) STRING
+
+name="Hilal Müleyke Yüksel"
+type(name)
+
+# 3) BOOLEAN
+True
+False
+type(True)
+97==95
+type(3==5)
+
+# 4) LISTS
+s=["usd","eur","cad"]
+type(s)
+
+# 5) DICTIONARY
+person={"name" : "Müleyke" , "age" : 25}
+type(person)
+
+# 6) TUPLE
+t=("python","java","c#")
+type(t)
+
+# 7) SETS
+c={"audi","volvo","honda"}
+type(c)
+
+# List, tuple, set and dictionaries are also known as Python Collections (Arrays).
+
